315b51c5352c5b6abb7f13a9917d049f3ad4d93f
poorvitusam <poorvi@onjax.com>
2017-10-16 10:44:44 -0400

    Accept valid arguments

--- airportSecurityState/src/airportSecurityState/driver/Driver.java
+++ airportSecurityState/src/airportSecurityState/driver/Driver.java
@@ -1,5 +1,21 @@
 package src.airportSecurityState.driver;
 
 public class Driver {
-
+	/**
+	 * main method
+	 * @param args
+	 */
+	public static void main(String args[]) {
+		if(args[0].equalsIgnoreCase("{arg0}") || args[1].equalsIgnoreCase("{arg1}") || args.length !=2) {
+			System.err.println("Invalid arguments");
+			System.exit(1);
+		}
+		
+		String inputFile = args[0];
+		String deleteFile = args[1];
+		
+		FileProcessor reader = new FileProcessor(inputFile, "read", null);;
+		FileProcessor writer = new FileProcessor(outputFile1, "write", "output");
+		
+	}
 }
